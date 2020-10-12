be4bd476e0bf9f42bd23c5a838952dbcb4497868
Eugene Yang <eugene87222@gmail.com>
2020-10-12 20:49:18 +0800

    update clf.ppy and preprocess.py

--- clf.py
+++ clf.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-import re
-import json
 import nltk
+import preprocess
 import numpy as np
-from sklearn.svm import SVC
-from xgboost import XGBClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import confusion_matrix, accuracy_score
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.metrics import confusion_matrix, accuracy_score
 
 
 def make_sentence(string):
@@ -53,42 +49,26 @@ def get_certain(data):
     return np.array(labels), np.array(contents)
 
 
-def fair_split(x, y, train_ratio=0.7):
-    x_train, x_test = train_test_split(x[y==0], train_size=train_ratio)
-    y_train = [0] * len(x_train)
-    y_test = [0] * len(x_test)
-    for i in range(1, 4):
-        split = train_test_split(x[y==i], train_size=train_ratio)
-        x_train = np.concatenate((x_train, split[0]))
-        y_train += [i] * len(split[0])
-        x_test = np.concatenate((x_test, split[1]))
-        y_test += [i] * len(split[1])
-    return x_train, x_test, np.array(y_train), np.array(y_test)
-
-
 if __name__ == '__main__':
-    data = json.load(open('structured.json', 'r', encoding='utf8'))
-    stopwords = set(nltk.corpus.stopwords.words('english'))
+    train = preprocess.parse_raw('./raw/train', savefile=False)
+    test = preprocess.parse_raw('./raw/test', savefile=False)
+    # stopwords = set(nltk.corpus.stopwords.words('english'))
 
-    labels, contents = get_certain(data)
-    # labels, contents = get_content(data)
-    contents_train, contents_test, y_train, y_test = fair_split(contents, labels, train_ratio=0.7)
+    labels_train, contents_train = get_content(train)
+    labels_test, contents_test = get_content(test)
 
-    vectorizer = TfidfVectorizer(stop_words=stopwords, max_df=0.9, min_df=1)
-    vectorizer.fit(contents_train)
-    feature_names = vectorizer.get_feature_names()
-    print(f'# of features: {len(feature_names)}')
+    # vectorizer = TfidfVectorizer(stop_words=stopwords, max_df=0.9, min_df=1)
+    # vectorizer.fit(contents_train)
+    # feature_names = vectorizer.get_feature_names()
+    # print(f'# of features: {len(feature_names)}')
 
-    x_train = vectorizer.transform(contents_train).toarray()
-    # clf = XGBClassifier()
-    clf = RandomForestClassifier(n_estimators=500, bootstrap=True, max_samples=0.6)
-    # clf = SVC(C=5e2)
-    clf.fit(x_train, y_train)
+    # x_train = vectorizer.transform(contents_train).toarray()
+    # clf = RandomForestClassifier(n_estimators=500, bootstrap=True, max_samples=0.6)
+    # clf.fit(x_train, labels_train)
+    # x_test = vectorizer.transform(contents_test).toarray()
+    # y_pred = clf.predict(x_test)
 
-    x_test = vectorizer.transform(contents_test).toarray()
-    y_pred = clf.predict(x_test)
-
-    print(f'gt: {y_test}')
-    print(f'pred: {y_pred}')
-    print(f'cm:\n{confusion_matrix(y_test, y_pred)}')
-    print(f'acc: {accuracy_score(y_test, y_pred)}')
+    # print(f'gt: {labels_test}')
+    # print(f'pred: {y_pred}')
+    # print(f'cm:\n{confusion_matrix(labels_test, y_pred)}')
+    # print(f'acc: {accuracy_score(labels_test, y_pred)}')
--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,19 @@
+# -*- coding: utf-8 -*-
 import os
 import re
 import json
+from os.path import normpath, basename
 
-DATA_DIR = './Case Presentation 1'
 CURRENT = 0
 NON = 1
 PAST = 2
 UNKNOWN = 3
 
-if __name__ == '__main__':
+
+def parse_raw(folder, savefile=True):
     result = []
 
-    for file in os.listdir(DATA_DIR):
+    for file in os.listdir(folder):
         if file.endswith('.txt'):
             if file.startswith('CURRENT'):
                 label = CURRENT
@@ -21,6 +23,8 @@ if __name__ == '__main__':
                 label = PAST
             elif file.startswith('UNKNOWN'):
                 label = UNKNOWN
+            else:
+                label = -1
 
             data = {
                 'filename': file,
@@ -30,34 +34,35 @@ if __name__ == '__main__':
             }
             key = None
             values = []
-            with open(f'{DATA_DIR}/{file}', 'r', encoding='utf8') as fp:
+            with open(f'{folder}/{file}', 'r', encoding='utf8') as fp:
                 for line in fp:
                     line = re.sub(r'\s+([.,:;\?!\])])', r'\1', line.strip())
                     line = re.sub(r'([\[(])\s+', r'\1', line)
                     line = re.sub(r'\s+(/)\s+', r'\1', line)
                     line = re.sub(r'"\s+(.+)\s+"', r'"\1"', line).strip()
 
+                    if line.lower() == 'cc:':
+                        continue
+
                     if line.endswith(':'):
                         if key is not None:
-                            data[key.lower()] = values[0] if len(values)==1 else values
+                            data[key.lower()] = values
                         key = line[:-1]
                         values = []
-
                     elif re.findall(r'^\d+\s*[a-zA-Z]*$', line) and key is None:
                         data['garbage_infos'] += line.split(' ')
-
                     elif line.startswith('*****'):
                         data['garbage_infos'].append(line)
-
                     elif re.findall(r'^[A-Z]+,\s+[A-Z]+\s+[0-9]+-', line):
                         data['garbage_infos'].append(line)
-
                     elif re.findall(r'\d+/\d+/\d+\s+\d+:\d+(:\d+)?\s+(AM|PM)$', line) and key is None:
                         data['times'].append(line)
-
                     elif key is not None:
                         values.append(line)
 
             result.append(data)
 
-    json.dump(result, open('structured.json', 'w', encoding='utf8'), indent=2, ensure_ascii=False)
+    if savefile:
+        json.dump(result, open(f'{basename(normpath(folder))}_structured.json', 'w', encoding='utf8'), indent=2, ensure_ascii=False)
+
+    return result
