23f2b2c8bf830cfbec39528ff65739ece426bee0
Eugene Yang <eugene87222@gmail.com>
2020-10-14 01:02:07 +0800

    add random forest method / add requirements.txt

--- classifier.py
+++ classifier.py
@@ -4,12 +4,17 @@ import re
 import json
 import string
 import operator
+import numpy as np
 from os.path import normpath, basename
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 CURRENT_SMOKE = 0
 NON_SMOKE = 1
 PAST_SMOKE = 2
 UNKNOWN = 3
+LABEL = ['current smoker', 'non-smoker', 'past smoker', 'unknown']
 
 
 def parse_raw(folder, savefile=True):
@@ -34,18 +39,15 @@ def parse_raw(folder, savefile=True):
                 'garbage_infos': [],
                 'times': []
             }
-            key = None
-            values = []
+            key, values = None, []
             with open(f'{folder}/{file}', 'r', encoding='utf8') as fp:
                 for line in fp:
-                    line = re.sub(r'\s+([.,:;\?!\])])', r'\1', line.strip())
+                    line = re.sub(r'\s+([.,:;\?!\])])', r'\1', line)
                     line = re.sub(r'([\[(])\s+', r'\1', line)
                     line = re.sub(r'\s+(/)\s+', r' \1 ', line)
                     line = re.sub(r'"\s+(.+)\s+"', r'"\1"', line).strip()
-
                     if line.lower() == 'cc:':
                         continue
-
                     if line.endswith(':'):
                         if key is not None:
                             data[key.lower()] = values
@@ -61,7 +63,6 @@ def parse_raw(folder, savefile=True):
                         data['times'].append(line)
                     elif key is not None:
                         values.append(line)
-
             result.append(data)
 
     if savefile:
@@ -70,13 +71,24 @@ def parse_raw(folder, savefile=True):
     return result
 
 
-def get_contents(data):
-    contents = []
+def get_content(data):
+    content = []
     for k, v in data.items():
         if k=='garbage_infos' or k=='filename' or k=='label':
             continue
-        contents += v
-    return contents
+        content += v
+    return content
+
+
+def get_content_merged(data):
+    content = ''
+    for k, v in data.items():
+        if k=='garbage_infos' or k=='filename' or k=='label':
+            continue
+        for elem in v:
+            content += elem
+            content += '. ' if elem[-1:].isalnum() else ' '
+    return content.strip()
 
 
 def load_words(path):
@@ -99,8 +111,8 @@ def get_relate_sentences(content, keywords):
 
 
 def predict(data, smoke_kw, neg_kw, stop_kw):
-    contents = get_contents(data)
-    sentences = get_relate_sentences(contents, smoke_kw+neg_kw+stop_kw)
+    content = get_content(data)
+    sentences = get_relate_sentences(content, smoke_kw+neg_kw+stop_kw)
     table = str.maketrans('', '', string.punctuation)
     count = {
         CURRENT_SMOKE: 0,
@@ -112,7 +124,6 @@ def predict(data, smoke_kw, neg_kw, stop_kw):
         if 'smoked' in sentence:
             count[PAST_SMOKE] += 1
             continue
-
         words = sentence.split()
         words = [w.translate(table) for w in words]
         has_neg = False
@@ -121,10 +132,8 @@ def predict(data, smoke_kw, neg_kw, stop_kw):
             if w in smoke_kw:
                 if has_neg:
                     count[NON_SMOKE] += 1
-                    # has_neg = False/
                 elif has_stop:
                     count[PAST_SMOKE] += 1
-                    # has_stop = False
                 else:
                     count[CURRENT_SMOKE] += 1
                 break
@@ -142,25 +151,51 @@ def predict(data, smoke_kw, neg_kw, stop_kw):
 
 
 if __name__ == '__main__':
+    # parse plain text files
     train = parse_raw('./raw/train', savefile=True)
     test = parse_raw('./raw/test', savefile=True)
 
+    # load custom dictionary
     smoke_kw = load_words('./data/smoke_kw.txt')
     neg_kw = load_words('./data/neg_kw.txt')
     stop_kw = load_words('./data/stop_kw.txt')
 
+    # rule-based classifier
+    print('\nRule-based classifier:\n--------------------')
+    print('Performance on training set:')
     correct = 0
-    for data in train:
-        pred = predict(data, smoke_kw, neg_kw, stop_kw)
-        print(f'{data["filename"]:25}, GT: {data["label"]}, pred: {pred}', end='')
-        print('' if pred==data['label'] else ' (wrong)')
-        if pred == data['label']:
+    for t in train:
+        pred = predict(t, smoke_kw, neg_kw, stop_kw)
+        if pred == t['label']:
             correct += 1
-    print(f'Acc on train: {100.*correct/len(train)}%')
+    print(f'Acc: {100.*correct/len(train)}%\n')
 
-    correct = 0
-    for data in test:
-        pred = predict(data, smoke_kw, neg_kw, stop_kw)
-        if pred == data['label']:
-            correct += 1
-    print(f'Acc on test: {100.*correct/len(test)}%')
+    print('Performance on testing set:')
+    for t in test:
+        pred = predict(t, smoke_kw, neg_kw, stop_kw)
+        print(f'{t["filename"]}, prediction: {LABEL[pred]}')
+    print('====================\n')
+
+    # tf-idf and random forest classifier
+    print('Random forest classifier (tf-idf score):\n--------------------')
+    contents_train, contents_test, y = [], [], []
+    for t in train:
+        contents_train.append(get_content_merged(t))
+        y.append(t['label'])
+    for t in test:
+        contents_test.append(get_content_merged(t))
+    y = np.array(y)
+
+    vectorizer = TfidfVectorizer(max_df=0.8, min_df=1)
+    vectorizer.fit(contents_train)
+    feature_names = vectorizer.get_feature_names()
+    print(f'# of features: {len(feature_names)}')
+    x_train = vectorizer.transform(contents_train)
+    x_test = vectorizer.transform(contents_test)
+    clf = RandomForestClassifier(n_estimators=500, max_features=0.6)
+    clf.fit(x_train, y)
+    pred = clf.predict(x_test)
+    print('Performance on testing set:')
+    for i, t in enumerate(test):
+        print(f'{t["filename"]}, prediction: {LABEL[pred[i]]}')
+    print()
